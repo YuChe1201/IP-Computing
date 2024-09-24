5daa888a709d271a823d0bd2da5b8a358bb8db9e
YuChe <liyuncheng1201@gmail.com>
2024-09-25 00:02:42 +0800

    /24

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import math
-a,b,c,d = map(int,input().split('.'))
-print(a,b,c,d)
-mask = int(input('/'))
-#p = input('切第幾段：')
-#o = input('取第幾段：')
-ip = int(input('第幾個可用IP：'))
 def class_D(a1,b1,c1,d1):
     n = int(math.pow(2,32-mask))
     d1 = int(d1/n)*n+ip
@@ -21,6 +14,13 @@ def class_D(a1,b1,c1,d1):
     netmask = ("255.255.255."+str(m))
     ans = (str(a1)+'.'+str(b1)+'.'+str(c1)+'.'+str(d1)+" "+netmask)
     return(ans)
+import math
+a,b,c,d = map(int,input().split('.'))
+print(a,b,c,d)
+mask = int(input('/'))
+#p = input('切第幾段：')
+#o = input('取第幾段：')
+ip = int(input('第幾個可用IP：'))
 
 if mask >= 24:
     print(class_D(a,b,c,d))
